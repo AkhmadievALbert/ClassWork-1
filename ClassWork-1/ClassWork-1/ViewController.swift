f78af102ba71c779798e80ea8091945d1c317ff8
AkhmadievALbert <a.akhmadiev@tinkoff.ru>
2021-09-20 19:14:26 +0300

    [init] static dispatch task

--- ClassWork-1/ClassWork-1/ViewController.swift
+++ ClassWork-1/ClassWork-1/ViewController.swift
@@ -12,8 +12,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        staticDispatchTest()
     }
 
-
+    
+    // MARK: StaticDispatch
+    
+    private func staticDispatchTest() {
+        let albaClassInstance = AlbaClass()
+        albaClassInstance.play() // Static
+        
+        let albaStructInstance = AlbaStruct()
+        albaStructInstance.play() // Static
+        
+        let albaSecondClassInstance = AlbaSecondClass()
+        albaSecondClassInstance.play() // Static
+    }
 }
 
